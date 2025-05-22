4ea4c32f43d213ea707b1af85dc9fd6b3fdc2009
pomelo925 <yoseph.huang@gmail.com>
2025-05-23 02:58:52 +0800

    fix: llm env

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 export DISPLAY=:0
 
 cd docker
-# docker compose -p discord-bot up default -d
-docker compose -p discord-bot up devel -d
+docker compose -p discord-bot up default -d
+# docker compose -p discord-bot up devel -d
